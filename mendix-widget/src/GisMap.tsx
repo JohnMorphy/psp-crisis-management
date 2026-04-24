7f3835737ebdd47eba564e333427fc6745cb36cb
Jan <jan.kaiser@3qcode.com>
2026-04-24 14:22:38 +0200

    WIP react component

--- mendix-widget/src/GisMap.tsx
+++ mendix-widget/src/GisMap.tsx
@@ -3,11 +3,23 @@ import { useEffect } from "react";
 
 export function GisMap() {
   useEffect(() => {
+    const WIDGET_BASE = "widgets/3qcode/gismap/assets";
+
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = `${WIDGET_BASE}/index.css`;
+    document.head.appendChild(link);
+
     const script = document.createElement("script");
-    script.src = "widgets/mywidget/assets/assets/index.js"; 
+    script.src = `${WIDGET_BASE}/index.js`;
     script.type = "module";
     document.body.appendChild(script);
+
+    return () => {
+      document.head.removeChild(link);
+      document.body.removeChild(script);
+    };
   }, []);
 
-  return <div id="root"></div>;
+  return <div id="root" style={{ width: "100%", height: "100%" }} />;
 }
